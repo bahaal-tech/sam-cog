e59ded5f975d581ccb6180918eb644bee7e66034
iamargentum <rajat.rj97@gmail.com>
2023-10-03 01:49:24 +0530

    Remove step to deepcopy image

--- predict.py
+++ predict.py
@@ -3,7 +3,6 @@
 
 import cv2
 import json
-import copy
 import base64
 import numpy as np
 from io import BytesIO
@@ -47,6 +46,8 @@ class Predictor(BasePredictor):
         positive_prompts = json.loads(positive_prompts)
         negative_prompts = json.loads(negative_prompts)
 
+        print("postive and negative promtps parsed to arrays")
+
         # add positive promtps and 1 in the same index in labels
         for point in positive_prompts:
             input_points.append(point)
@@ -74,19 +75,17 @@ class Predictor(BasePredictor):
 
         # reference for "cutting" out segmented images -
         # https://github.com/facebookresearch/segment-anything/issues/221#issuecomment-1614280903
-        
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         for mask in masks:
-            # creating a deepcopy of the image
-            image_copy = copy.deepcopy(image)
             # converting false parts to white color
-            image_copy[mask==False] = [255, 255, 255]
+            image[mask==False] = [255, 255, 255]
 
             # add image to segmented images by converting them to b64
             segmented_images.append(
                 base64.b64encode(
-                    image_copy.tobytes()
+                    image.tobytes()
                 )
             )
 
